3b3857f3bd1b632904d15e6cb0a4c1bfcbf054b2
dhruv <dhruvjain137@gmail.com>
2025-04-16 11:56:20 +0530

    15April-Buttons

--- App.tsx
+++ App.tsx
@@ -1,131 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+import React from "react";
+import { View,Text, StyleSheet, StatusBar, Button, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback} from "react-native";
+function App(){
+  const alertFunc =()=>{
+    console.log("Button Preseed");
   };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+    <View style={{flex:1,justifyContent:'center',alignItems:'center',gap:20}}>
+      <TouchableHighlight underlayColor={'blue'} 
+      onPress={alertFunc}>
+        {/* gives a underlay color when button is pressed */}
+        <View style={styles.btnColor}>
+          <Text>Press Me</Text>
         </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      </TouchableHighlight>
+      <TouchableOpacity>
+        {/* gives opacity to it when clicked --- initally white color*/}
+        <View style={styles.btnColor}>
+          <Text>Press Me</Text>
         </View>
-      </ScrollView>
+      </TouchableOpacity>
+      <TouchableNativeFeedback> 
+        {/* gives rippling feedback */}
+        <View style={styles.btnColor}>
+          <Text>Press Me</Text>
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableWithoutFeedback>
+        {/* for giving the simple button with no effect */}
+        <View style={styles.btnColor}>
+          <Text>Press Me</Text>
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+const styles = StyleSheet.create({
+  btnColor:{
+    backgroundColor:'red',
+    width:200,
+    height:50,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  appheight:{
+    marginTop: StatusBar.currentHeight,
+  }
+});
