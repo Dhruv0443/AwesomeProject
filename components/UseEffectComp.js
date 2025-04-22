ecb94a4dcd8d15ab531055a9626671cbec68b7fd
dhruv <dhruvjain137@gmail.com>
2025-04-22 11:04:29 +0530

    22April -Calling api with scrollVeiw

--- components/UseEffectComp.js
+++ components/UseEffectComp.js
@@ -1,4 +1,4 @@
-import { Alert, Button, StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Loading from './Loading';
 const UseEffectComp = () => {
@@ -12,12 +12,31 @@ const UseEffectComp = () => {
       }
       callingApi();
     },[]);
-
   return (
-    <View style={{width:'100%', height:'100%'}}>
-    {products.length===0 ? <Loading/>: <Text>Hello</Text>}
-    </View>
+        <ScrollView style={{width:'100%',height:"100%"}}>  
+            {products.length===0 ? <Loading /> : 
+                   products.map(item=>{
+                    return (
+                    <View style={styles.viewStyle}>
+                        <Image source={{uri:item.images[0]}} style={{width:100,height:100}}/>
+                        <Text style={{fontWeight:'900', paddingBottom:10}}>{item.title}</Text>
+                        <Text>{item.description}</Text>
+                        <Text>Price: ${item.price}</Text>
+                    </View>
+                    );
+                })         
+            }
+        </ScrollView>
+
   )
 }
 export default UseEffectComp;
-const styles = StyleSheet.create({})
+
+const styles=StyleSheet.create({
+    viewStyle:{
+        padding:10,
+        borderWidth:1,
+        margin:10,
+        borderRadius:10,
+      }
+});
